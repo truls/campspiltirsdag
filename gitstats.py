947c6c37071198b19602a65a02b531e84b9da936
Truls Asheim <truls@diku.dk>
2011-04-16 23:54:39 +0200

    Rewrote using the github json api. It actually works now and attributes changes correctly

--- gitstats.py
+++ gitstats.py
@@ -5,127 +5,162 @@ from json import JSONDecoder
 import urllib2
 from subprocess import Popen, PIPE, call
 import codecs
+from dateutil.parser import parse as dateparser
+from datetime import timedelta
 import os
 import sys
-
-
-class Directory:
-    def __init__(self):
-        self.dirs = []
-
-    def pushd(self, dir):
-        self.dirs.append(os.getcwd())
-        os.chdir(dir)
-
-    def popd(self):
-        os.chdir(self.dirs.pop())
-
+import cPickle as pickle
 
 json = JSONDecoder()
-d = Directory()
 
-users = ['Athas', 'blackplague', 'jlouis', 'kjmikkel', 'Munksgaard', 'Munter', 'NajaWulff', 'omegahm', 'roschnowski ', 'svip', 'truls']
+users = {'Athas': {'commits': 0},
+         'blackplague': {'commits': 0},
+         'jlouis': {'commits': 0},
+         'kjmikkel': {'commits': 0},
+         'Munksgaard': {'commits': 0},
+         'Munter': {'commits': 0},
+         'NajaWulff': {'commits': 0},
+         'omegahm': {'commits': 0},
+         'roschnowski': {'commits': 0},
+         'svip': {'commits': 0},
+         'truls': {'commits': 0},
+         }
 
 commit = {}
 
+#lanstart = 1302883200
+lanstart = dateparser("2011-04-15T16:00:00+01:00")
 
+lcaseusers = []
+for user in users.iterkeys():
+    lcaseusers.append(user.lower())
 
+# Load users and their commits
+if os.path.exists("commits.pickle"):
+    with open("commits.pickle") as f:
+        users = pickle.load(f)
 
-# Basepath. Set to pwd if not specifie
+#with open("users.pickle")
+
+# Basepath. Set to pwd if not specified
 cwd = os.getcwd()
 if len(sys.argv) > 1:
     cwd = sys.argv[1]
-    
 
 # Create directories and get list of user repostiroes
 
-for user in users:
-    userstate = []
-    os.chdir(cwd)
-    #d.pushd(cwd)
-    try:
-        os.mkdir(user)
-        print "Creating dir for ", user
-    except OSError:
-        pass
-
+for user in users.iterkeys():
+    print "Processing user:", user
     
-    try:
-        # Get user fullname
-        with open(user + ".name", "r") as f:
-            username = f.read().strip()
-    except:
-        with codecs.open(user + ".name", "w", encoding="utf-8") as f:
-            userinfo = urllib2.urlopen("http://github.com/api/v2/json/user/show/" + user)
-            userjson = json.decode(userinfo.read())
-            try:
-                username = userjson["user"]["name"]
-            except:
-                username = "user"
-            f.write(username)
-
-
-
-    commit[user] = 0
-
-    os.chdir(os.path.join(cwd, user))
-
+    # Get all repos for suer
     ghjson = urllib2.urlopen("http://github.com/api/v2/json/repos/show/" + user)
     #gh = ghjson
     repos = json.decode(ghjson.read())
     #print repos
     for repo in repos["repositories"]:
+        reponame = repo["name"]
+        print "Processing repo:,", reponame
+
         try:
-            print "Current dir:", os.getcwd()
-            # Get latest commit
-            with open(repo["name"] + ".status", "r") as f:
-                latestcommit = f.read().strip()
-            print "Loaded saved lastest commit:", latestcommit
-        except IOError, e:
-            #print e
-            if not e.errno == 2:
-                raise
-            latestcommit = ""
-            print "No saved lastest commit"
-        
-        try:
-            commitinfo = urllib2.urlopen("http://github.com/api/v2/json/commits/list/" + user + "/" + repo["name"] + "/master")
-            commitjson = json.decode(commitinfo.read())
-            currentcommit = commitjson["commits"][0]["id"]
-            print "Github says current commit is", currentcommit
+            users[user][reponame]
         except:
-            print "Fetching latest commit from ghithub failed. WTF??"
-            currentcommit = "failed"
- 
-        if latestcommit != currentcommit:
-
-            print "********New commit. Updating"
-
-            with open(repo["name"] + ".status", "w") as f:
-                f.write(currentcommit)
+            users[user][reponame] = {}
         
-            #print repo
-            if not os.path.isdir(os.path.join(os.getcwd(), repo["name"])):
-                print "Cloning repository ", repo["name"]
-                call(["git", "clone", repo["url"]])
-            else:
-                print "Pulling repository ", repo["name"]
-                ocwd = os.getcwd()
-                os.chdir(os.path.join(ocwd, repo["name"]))
-                call(["git", "pull"])
-                os.chdir(ocwd)
 
-        ocwd = os.getcwd()
-        os.chdir(os.path.join(ocwd,repo["name"]))
-        print "Commit count for", user, "is now", commit[user]
-        commit[user] += len(Popen(["git", "log", "--pretty=format:\"%H\"", "--since=1302883200"], stdout=PIPE).communicate()[0].split())
-        print "Commit count for", user, "is now", commit[user]
-        os.chdir(ocwd)
-        #"--author=\"%s\"" % username
-os.chdir(cwd)
+        ghjson = urllib2.urlopen("http://github.com/api/v2/json/repos/show/" + user + "/" + reponame + "/branches")
+        branches = json.decode(ghjson.read())
+        #print branches["branches"]
+        for branch, head in branches["branches"].iteritems():
+            print "Processing branch", branch
+
+            try:
+                users[user][reponame][branch]
+            except:
+                users[user][reponame][branch] = ""
+
+            
+            oldhead = users[user][reponame][branch]
+            if oldhead == head:
+                print "No updates in %s/%s" % (reponame, branch)
+                continue
+            print "Updates for repository", reponame, "old head:", oldhead, "new head:", head
+            users[user][reponame][branch] = head
+
+            #currentdate = lanstart + timedelta(1)
+            page=1
+
+            stoploop = False
+            commitpage = 1
+
+            # Loop while: Commit ID doesn't match old id
+            #             commit time isn't older than the start of DIKULAN
+            #while currentdate > lanstart and \
+
+            # There are possibly new commits. Recount
+            while True:
+                
+                try:
+                    url = "http://github.com/api/v2/json/commits/list/" + \
+                          user + "/" + reponame + "/" + branch + "?page=" + str(page)
+                    print "Fetching URL:", url
+                    decoder = codecs.getdecoder('utf-8')
+                    commitinfo = urllib2.urlopen(url).read()
+                    #print commitinfo
+                    try:
+                        commitinfo =  decoder(commitinfo)[0]
+                    except UnicodeDecodeError:
+                        print "FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU"
+                        # Let's try next page to see if that's any better
+                        page += 1
+                        continue
+                    
+                    commits = json.decode(commitinfo)["commits"]
+                    #print "Github says current commit is", currentcommit
+                except urllib2.HTTPError:
+                    print "Fetching commits from github failed. That we have reached the end of the list."
+                    stoploop = True
+                
+                for commit in commits:
+                    #print commit
+                    ctime = dateparser(commit["committed_date"])
+                    print ctime, "<",  lanstart, ctime < lanstart
+
+                    print oldhead, "==", commit["id"], oldhead == commit["id"]
+                    if ctime < lanstart or oldhead == commit["id"]:
+                        print "stoploop"
+                        stoploop = True
+                        break 
+
+                    comitter = commit["committer"]["login"]
+                    if comitter == '':
+                        # We need something. Lets try using name instead
+                        print "Login was empty... Wierd... Using name instead"
+                        comitter = commit["committer"]["name"].lower()
+                    if not comitter.lower() in lcaseusers:
+                        print "Not our commit. This was comitted by", comitter
+                        if comitter.strip() == "":
+                            print commit
+                        continue
+
+                    # Increment commitcount
+                    users[comitter]["commits"] += 1
+                    print "One more commit for %s :)" % comitter
+
+                if stoploop:
+                    break
+                page += 1
+ 
+
+
+print "Commit counts"
 with open("commitcount.txt", "w") as f:
-    for name, commits in commit.iteritems():
-        f.write("%s %d\n" % (name, commits))
+    for name in users.iterkeys():
+        userline = "%s %d\n" % (name, users[name]["commits"])
+        f.write(userline)
+        print userline
+        
 
-print commit    
+print "Pickling sate"
+with open("commits.pickle", "w") as f:
+    pickle.dump(users, f)
 
