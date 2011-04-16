bef95b9083cd058e9299270a16458f47cc7f49eb
Svip <svip@diku.dk>
2011-04-16 20:48:17 +0200

    liberalaksemagt init

---
+++ liberalaksemagt/http/index.py
@@ -0,0 +1,13 @@
+
+content = """<!doctype html>
+<html>
+<head>
+<title>Jeg er doven</title>
+</head>
+<body>
+<h1>Svinene er her</h1>
+</body>
+</html>"""
+
+print "Content-Type: text/html; charset=utf-8\n"
+print content
